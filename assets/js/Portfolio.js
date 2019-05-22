bb6624d84cb819c093a816b4094ae8a7df1548be
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-22 01:39:12 -0400

    Updated Portfolio page. Still needs some css styling

--- assets/js/Portfolio.js
+++ assets/js/Portfolio.js
@@ -1,69 +1,69 @@
-class TabLink {
-  constructor(tabElement){
-    // assign this.tabElement to the tabElement DOM reference
-    this.tabElement = tabElement;
-    
-    // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = this.tabElement.dataset.tab;
-    
-    // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-    
-    // Check to see if this.tabData is equal to 'all'
-    if( this.tabData === 'all' ){
-      // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll(`.card`);
-    } else {
-      // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
-    } 
+class Topic {
+  constructor(element){
+    // assign this.element to DOM reference
+    this.element = element;
 
-     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map((card) => new TabCard(card));
+    // Check which data-set was selected and get the value
+    if (typeof this.element.dataset.theme !== "undefined") { 
+      this.set = "theme";
+      this.dataTopic = this.element.dataset.theme; 
+    } else if (typeof this.element.dataset.location !== "undefined") { 
+      this.set = "location";
+      this.dataTopic = this.element.dataset.location; 
+    } else if (typeof this.element.dataset.planner !== "undefined") { 
+      this.set = "planner";
+      this.dataTopic = this.element.dataset.planner; 
+    }
+
+    // Check to see if the Topic topic is equal to 'all'
+    if ( this.dataTopic === 'all' ){
+      // If `all` is true, display all topics
+      this.articles = document.querySelectorAll(`.content-container article`);
+      } else {
+      // else if `all` is false, only select the themes with matching selection
+      this.articles = document.querySelectorAll(`.content-container article[data-${this.set}="${this.dataTopic}"]`);
+    }
+
+     // Convert selected topics to displayTopic
+    this.articles = Array.from(this.articles).map((topic) => new displayTopic(topic));
 
     // Add a click event that invokes this.selectTab
-    this.tabElement.addEventListener('click', () => this.selectTab());
+    this.element.addEventListener('click', () => this.selectTab());
   }
 
   selectTab(){
-    // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll('.tab'); console.log(`${tabs}`);
-    
-    // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach(tab => tab.classList.remove('active-tab'));
 
-    // Select all of the elements with the .card class on them
-    const cards = document.querySelectorAll(`.card`);
-
-    // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(card => card.style.display = "none");
+    // Select classes named .topic
+    const allTopics = document.querySelectorAll('.topic');
     
-    // Add a class of ".active-tab" to this.tabElement
-    this.tabElement.classList.add('active-tab');
+    // Remove the .active class from each element
+    allTopics.forEach(e => e.classList.remove('active'));
+
+    // Select all articles
+    const articles = document.querySelectorAll(`.content-container article`);
+
+    // Set display style for each article to 'none'
+    articles.forEach(e => e.style.display = "none");
+    
+    // Add a class of ".active" to selected element
+    this.element.classList.add('active');
   
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    this.cards.forEach(card => card.selectCard());
+    // Display selected articles
+    this.articles.forEach(e => e.selectTopic());
   }
 }
 
-class TabCard {
-  constructor(cardElement){
-    // Assign this.cardElement to the cardElement DOM reference
-    this.cardElement = cardElement;
+class displayTopic {
+  constructor(element){
+    // Assign this.element from this.display (selected topics to displayTopic)
+    this.element = element;
   }
-  selectCard(){
-    // Update the style of this.cardElement to display = "flex"
-    this.cardElement.style.display = "flex";
+  selectTopic(){
+    // Update the style of selected topics to display = "flex"
+    this.element.style.display = "block";
   }
 
 }
 
-/* START HERE: 
-
-- Select all classes named ".tab" and assign that value to the tabs variable
-
-- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
-*/
-let tabs = document.querySelectorAll('.tab').forEach( tab => new TabLink(tab));
+// Select classes named .topic and return a new instance of Topic
+let topics = document.querySelectorAll('.topic').forEach( topic => new Topic(topic));
