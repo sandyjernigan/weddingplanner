0ee74fbf780808fecc745d94dd08522a8eb05308
sandyjernigan <sandy.jernigan@gmail.com>
2019-05-22 21:58:46 -0400

    Updated Nav to be responsive

---
+++ assets/js/Nav.js
@@ -0,0 +1,7 @@
+function navBarDropDown() {
+  //let nav = document.getElementById("mainNav");
+    nav.classList.toggle("responsive");
+    console.log("clicked");
+}
+let nav = document.getElementById("mainNav");
+console.log(nav.classList);
